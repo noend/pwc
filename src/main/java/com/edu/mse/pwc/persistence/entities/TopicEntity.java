81476195461a5e5b18ce54aa6b4532ccc1899317
ivo.rusev <ivo.rusev@musala.com>
2021-04-03 15:08:09 +0300

    Added replies.

--- src/main/java/com/edu/mse/pwc/persistence/entities/TopicEntity.java
+++ src/main/java/com/edu/mse/pwc/persistence/entities/TopicEntity.java
@@ -3,35 +3,23 @@ package com.edu.mse.pwc.persistence.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.UniqueConstraint;
-import java.util.Date;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "topics")
-public class TopicEntity {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @CreationTimestamp
-    @Column(name = "created_at")
-    private Date createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "modified_at")
-    private Date modifiedAt;
+public class TopicEntity extends BaseEntity {
 
     @Column(unique = true)
     private String title;
+
+    @OneToMany(mappedBy = "topic", fetch = FetchType.EAGER)
+    private List<ReplyEntity> reply;
 
 }
